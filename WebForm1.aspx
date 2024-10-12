577d862bc2f83e118a1f3ae81882c09d8a1d5504
Mukesh Saini <156110941+saini-mukesh@users.noreply.github.com>
2024-10-12 15:57:05 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView runat="server" ID="GridView1"></asp:GridView>
+            <asp:TextBox runat="server" ID="TextBox1"></asp:TextBox>      
+            <asp:TextBox runat="server" ID="TextBox2"></asp:TextBox>
+            <asp:TextBox runat="server" ID="TextBox3"></asp:TextBox>
+            <asp:TextBox runat="server" ID="TextBox4"></asp:TextBox>  
+            <asp:Button ID="Button1" runat="server" Text="save" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" Text="Delete" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="update" OnClick="Button3_Click" />
+            
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace WebApplication5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static string connString = ConfigurationManager.ConnectionStrings["EmpDBConnectionString"].ConnectionString;
+
+        DataClasses1DataContext context = new DataClasses1DataContext(connString);
+        void LoadGrid()
+        {
+            var data = from p in context.Employees
+                       where p.EmpSalary > 20000
+                       select p;
+            GridView1.DataSource = data;
+            GridView1.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var data = from p in context.Employees
+                       where p.EmpSalary > 20000
+                       select p;
+            GridView1.DataSource = data;
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Employee em = new Employee();
+            em.EmpId = int.Parse(TextBox1.Text);
+            em.EmpName = TextBox2.Text;
+            em.EmpSalary = int.Parse(TextBox3.Text);
+            em.DeptId = int.Parse(TextBox4.Text);
+            context.Employees.InsertOnSubmit(em);
+            context.SubmitChanges();
+            LoadGrid();
+
+
+
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Employee emp = context.Employees.First(p => p.EmpId == int.Parse(TextBox1.Text));
+            
+            context.Employees.DeleteOnSubmit(emp);
+            context.SubmitChanges();
+            LoadGrid();
+
+
+
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            var data = context.Employees.Where(p => p.EmpId == int.Parse(TextBox1.Text)).ToList();
+            data[0].EmpName = TextBox2.Text;
+            data[0].EmpSalary = int.Parse(TextBox3.Text);
+            data[0].DeptId = int.Parse(TextBox4.Text);
+            context.SubmitChanges();
+            LoadGrid();
+
+
+
+
+        }
+       
+    }
+}
